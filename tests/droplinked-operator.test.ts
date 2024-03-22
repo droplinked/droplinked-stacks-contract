490be3811653d8d56e53cc0dd5658c50dfd4be99
Erfan Yeganegi <76695726+erfanyeganegi@users.noreply.github.com>
2024-03-22 07:31:31 +0330

    chore: add droplinked-destination tests

--- tests/droplinked-operator.test.ts
+++ tests/droplinked-operator.test.ts
@@ -35,3 +35,38 @@ describe("'set-droplinked-admin' function", () => {
 		)
 	})
 })
+
+describe("'set-droplinked-destination' function", () => {
+	it('ensures only current admin can call function', () => {
+		const response = simnet.callPublicFn(
+			'droplinked-operator',
+			'set-droplinked-destination',
+			[Cl.principal('ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5')],
+			'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5'
+		)
+
+		expect(response.result).toBeErr(Cl.uint(100))
+	})
+
+	it('ensures current admin call function and change destination', () => {
+		const response = simnet.callPublicFn(
+			'droplinked-operator',
+			'set-droplinked-destination',
+			[Cl.principal('ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5')],
+			'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM'
+		)
+
+		expect(response.result).toBeOk(Cl.bool(true))
+
+		const adminResponse = simnet.callReadOnlyFn(
+			'droplinked-operator',
+			'get-droplinked-destination',
+			[],
+			'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM'
+		)
+
+		expect(adminResponse.result).toBePrincipal(
+			'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5'
+		)
+	})
+})
