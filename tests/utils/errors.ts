3f72c25a018f65c1086c9df2655aaddb74cca60e
Erfan Yeganegi <76695726+erfanyeganegi@users.noreply.github.com>
2024-03-24 13:30:31 +0330

    chore: add create-product function errors

--- tests/utils/errors.ts
+++ tests/utils/errors.ts
@@ -1,3 +1,13 @@
 import { Cl } from '@stacks/transactions'
 
-export const DroplinkedOperatorOnly = Cl.uint(100)
+export const BaseErrors = {
+	DROPLINKED_OPERATOR_ONLY: Cl.uint(100),
+}
+
+export const OperatorErrors = {
+	PUBLISHER_ONLY: Cl.uint(201),
+	PRODUCER_ONLY: Cl.uint(201),
+	INVALID_PRICE: Cl.uint(300),
+	INVALID_COMMISSION: Cl.uint(301),
+	INVALID_TYPE: Cl.uint(302),
+}
